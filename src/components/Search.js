7ede1ebb6205744b8e7c1e8abd73da8c03a6e096
Torre <tostaylo1@gmail.com>
2019-01-07 21:49:29 -0600

    Added react-vis

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import {
+  XYPlot,
+  XAxis,
+  YAxis,
+  VerticalGridLines,
+  HorizontalGridLines,
+  VerticalBarSeries,
+  DiscreteColorLegend,
+} from 'react-vis'
 import './Search.css'
 
 class Search extends Component {
   state = {
     data: [],
-    input: 'http://www.',
+    input: 'https://www.',
     query: [],
+    metrics: [
+      'first-contentful-paint',
+      'first-meaningful-paint',
+      'speed-index',
+      'first-cpu-idle',
+      'interactive',
+      'estimated-input-latency',
+    ],
   }
 
-  getData = query =>
+  getData = () =>
     axios
       .get('http://localhost:8001/lighthouse', {
         params: {
@@ -25,7 +42,8 @@ class Search extends Component {
       })
 
   render() {
-    const { input, query, data } = this.state
+    const { input, query, data, metrics } = this.state
+    const legendItems = data.map(({ finalUrl }) => finalUrl)
     return (
       <div className="search">
         <input
@@ -33,38 +51,63 @@ class Search extends Component {
           value={input}
           onChange={event => {
             event.persist()
-            this.setState(state => ({
+            this.setState(() => ({
               input: `${event.target.value}`,
             }))
           }}
           type="text"
         />
         <button
+          type="button"
           onClick={() =>
             this.setState(state => ({
               query: [...state.query, state.input],
-              input: `http://www.`,
+              input: `https://www.`,
             }))
           }
         >
           Add URL
         </button>
-        <button onClick={() => this.getData(query)}>Run Lighthouse</button>
+        <button type="button" onClick={() => this.getData()}>
+          Run Lighthouse
+        </button>
         <ul>
           {query.length >= 1 &&
             query.map(item => {
               return <li key={item}>{item}</li>
             })}
         </ul>
-        <ul>
-          {data.length >= 1 &&
-            data.map(item => (
-              <li>
-                {item.categories.performance.id}
-                {item.categories.performance.score}
-              </li>
-            ))}
-        </ul>
+
+        <DiscreteColorLegend height={200} width={300} items={legendItems} />
+        <XYPlot height={700} width={1200} xType="ordinal">
+          <VerticalGridLines />
+          <HorizontalGridLines />
+          <XAxis title="metric" />
+          <YAxis title="ms" />
+          {data.map(item => {
+            return (
+              <VerticalBarSeries
+                key={item.finalUrl}
+                data={metrics.map(metric => {
+                  console.log(item.audits[metric])
+                  return {
+                    x: item.audits[metric].title,
+                    y: item.audits[metric].rawValue,
+                  }
+                })}
+              />
+            )
+          })}
+          {/* <VerticalBarSeries
+            key="blah"
+            data={[
+              {
+                x: 10,
+                y: 20,
+              },
+            ]}
+          /> */}
+        </XYPlot>
       </div>
     )
   }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,31 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { Link } from 'gatsby'
+import Helmet from 'react-helmet'
 import Search from '../components/Search'
 import Layout from '../components/layout'
 import Image from '../components/image'
 import SEO from '../components/seo'
 
 const IndexPage = () => (
-  <Layout>
-    <Search />
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+  <Fragment>
+    <Helmet>
+      <link
+        rel="stylesheet"
+        href="https://unpkg.com/react-vis/dist/style.css"
+      />
+    </Helmet>
+    <Layout>
+      <Search />
+      <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  </Fragment>
 )
 
 export default IndexPage
