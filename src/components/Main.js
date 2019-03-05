7eb6abcf9897eb372651474b3c3d4d809d3c7cb5
Torre <tostaylo1@gmail.com>
2019-03-04 19:05:54 -0600

    env variables for firebase config

--- src/components/Main.js
+++ src/components/Main.js
@@ -24,6 +24,15 @@ import {
   SearchTerm,
 } from './MainStyles'
 
+const config = {
+  apiKey: process.env.GATSBY_FIREBASE_API_KEY,
+  authDomain: process.env.GATSBY_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.GATSBY_FIREBASE_DATABASE_URL,
+  projectId: process.env.GATSBY_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.GATSBY_FIREBASE_GOOGLE_STORAGE_BUCKET,
+  messagingSenderId: process.env.GATSBY_FIREBASE_MESSAGING_SENDER_ID,
+}
+
 firebase.initializeApp(config)
 
 const initialState = {
@@ -162,6 +171,7 @@ class Main extends Component {
   retrieveDbReports = () => {
     const db = firebase.database()
     const ref = db.ref(`lighthouseReports/Home`)
+
     ref.on(
       'value',
       snapshot => {
@@ -203,6 +213,7 @@ class Main extends Component {
       timelineResults,
       radioIds,
     } = this.state
+
     const radioIdentifiers = [
       {
         value: 'URL Search',
