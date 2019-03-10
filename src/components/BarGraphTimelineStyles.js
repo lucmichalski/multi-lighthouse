b796284b051dcc79c843644b6e04052e2c50422f
Torre <tostaylo1@gmail.com>
2019-03-10 15:21:05 -0500

    aspect ratio of graphs

--- src/components/BarGraphTimelineStyles.js
+++ src/components/BarGraphTimelineStyles.js
@@ -2,17 +2,19 @@ import styled from 'styled-components'
 
 export const OuterGraph = styled.div`
   box-sizing: border-box;
-  width: 100%;
+  width: 100vw;
+  height: 75vw;
   padding: 0 15px;
   margin-bottom: 25px;
 
   @media (min-width: 900px) {
-    width: 49%;
+    width: 43vw;
+    height: 33vw;
   }
 `
 export const InnerGraph = styled.div`
   width: 100%;
-  height: 300px;
+  height: 100%;
 `
 
 export const Title = styled.h2`
